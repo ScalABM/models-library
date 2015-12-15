154a344c91b7ef6ab0205b4475e80296908c9400
David R. Pugh <drobert.pugh@gmail.com>
2015-12-15 15:05:04 +0000

    Filled out methods for Liquidity Demander.

--- src/main/scala-2.11/LiquidityDemander.scala
+++ src/main/scala-2.11/LiquidityDemander.scala
@@ -1,12 +1,28 @@
 
+import akka.agent.Agent
+
 import markets.orders.market.MarketOrderLike
+import markets.tickers.Tick
 import markets.tradables.Tradable
+
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.ExecutionContext.Implicits.global
 
 
 /** A Mixin trait providing behavior necessary to generate and submit `MarketOrderLike` orders. */
 trait LiquidityDemander {
   this: TradingActor =>
 
-  def generateMarketOrder(tradable: Tradable): MarketOrderLike
+  def generateMarketOrder(tradable: Tradable, ticker: Agent[Tick]): MarketOrderLike
+
+  def submitMarketOrder(): Unit
+
+  def scheduleMarketOrder(initialDelay: FiniteDuration): Unit = {
+    context.system.scheduler.scheduleOnce(initialDelay)(submitMarketOrder())
+  }
+
+  def scheduleMarketOrder(initialDelay: FiniteDuration, interval: FiniteDuration): Unit = {
+    context.system.scheduler.schedule(initialDelay, interval)(submitMarketOrder())
+  }
 
 }
