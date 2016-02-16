a2335186d312f3c23378cafae001a19cb75630b6
David R. Pugh <drobert.pugh@gmail.com>
2016-02-16 14:15:59 +0000

    Attempt to fix coveralls scripts.

--- .travis/generate_coverage.sh
+++ .travis/generate_coverage.sh
@@ -3,7 +3,6 @@
 echo -e "Running unit tests and generating coverage statistics for gode-sunder example model...\n"
 cd ./gode-sunder
 sbt clean coverage test
-sbt coveralls
 cd ..
 echo -e "...finished running unit tests and generating coverage statistics!\n"
 
