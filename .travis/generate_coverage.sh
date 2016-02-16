473f4415ad46181dc64dc0f2cbe8811ed93d377f
David R. Pugh <drobert.pugh@gmail.com>
2016-02-16 12:59:44 +0000

    Minor modifications to generate coverage script.

--- .travis/generate_coverage.sh
+++ .travis/generate_coverage.sh
@@ -3,5 +3,5 @@
 echo -e "Running unit tests for gode-sunder example model...\n"
 cd ./gode-sunder
 sbt clean coverage test
-echo -e "...completed unit tests.\n"
+echo -e "...completed unit tests for the gode-sunder example model.\n"
 
