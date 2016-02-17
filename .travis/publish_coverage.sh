ed411c081ce46c64887cf2f215773d12de581a93
David R. Pugh <drobert.pugh@gmail.com>
2016-02-17 11:31:11 +0000

    Updated publish coverage script.

--- .travis/publish_coverage.sh
+++ .travis/publish_coverage.sh
@@ -6,7 +6,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
     cd ./gode-sunder
     sbt coveralls
     sbt coverageReport
-    cd ..
 
     echo -e "...publishing coverage reports...\n"
 
@@ -16,17 +15,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; th
 
     cd gh-pages
 
-    # get rid of old coverage stats
-    git rm -rf ./coverage
-
     # copy over the new coverage stats
-    mkdir -p ./coverage/gode-sunder
-    cp -Rf ../target/scala-2.11/scoverage-report ./coverage/gode-sunder
+    mkdir -p ./coverage/gode-sunder/latest
+    cp -Rf ../target/scala-2.11/scoverage-report ./coverage/gode-sunder/latest
 
     # push to github!
-    git add -f .
+    git add ./coverage
     git commit -m "Latest coverage report on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-    git push -fq origin gh-pages > /dev/null
+    git push origin gh-pages > /dev/null
 
     echo -e "Published coverage reports to gh-pages!\n"
 fi
